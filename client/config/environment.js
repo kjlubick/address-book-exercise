e91ae1d8397d9c8b79a7834602127b51eff1764b
Eric Lifka <eric.lifka@gmail.com>
2015-04-12 23:02:14 -0400

    fix config issues with client naming and proxy requests over to api server

--- client/config/environment.js
+++ client/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'address-book',
+    modulePrefix: 'address-book-client',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
