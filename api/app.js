1f50ef4d054435979025c8540565ac5b348c6879
Eric Lifka <eric.lifka@gmail.com>
2015-04-16 23:01:29 -0400

    implement endpoint for loading a single person by id

--- api/app.js
+++ api/app.js
@@ -1,25 +1,40 @@
 require('colors');
 var path = require('path');
 var express = require('express');
-var people = require(path.join(__dirname, 'data/people.json'));
+
+var peopleBlob = require(path.join(__dirname, 'data/people.json'));
+var people = peopleBlob['people'];
 
 var app = express();
-
-
-app.use('/mockup/', express.static(path.join(__dirname, 'mockup')));
-app.get('/api/people', function(req, res) {
-    res.end(JSON.stringify(people, null, '    '));
-});
+app.get('/api/people/:id', getPersonById);
+app.get('/api/people', getAllPeople);
 
 var HTTP_PORT = 8080;
-
 app.listen(HTTP_PORT, function(err) {
     if (err) {
         throw err;
     }
 
     console.log(('HTTP server listening on port ' + HTTP_PORT).green);
-
-    console.log('Mockup:'.bold + ' http://localhost:' + HTTP_PORT + '/mockup/');
     console.log('People data:'.bold + ' http://localhost:' + HTTP_PORT + '/api/people');
 });
+
+function getPersonById(req, res) {
+    var id = parseInt(req.params.id, 10);
+    var filtered = people.filter(function (person) {
+        return person.id === id;
+    });
+
+    if (filtered.length === 0) {
+        res.sendStatus(404);
+    }
+    else {
+        res.json({
+            person: filtered[0]
+        });
+    }
+}
+
+function getAllPeople(req, res) {
+    res.json(peopleBlob);
+}
